8013213e1db356b72fb3115ab1b59632de34f03a
Jonathan Luco <Jonathan.luco44@gmail.com>
2024-11-18 16:50:40 +0100

    Fin de l'ensemble des routes de la template

--- Dev/templateProject/src/app/app.routes.ts
+++ Dev/templateProject/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import {Routes} from '@angular/router';
+import {Component} from '@angular/core';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,14 @@ export const routes: Routes = [
   {
     path: "home", // Le chemin
     loadComponent: () => import('../views/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path:"generic",
+    loadComponent: () => import('../views/generic/generic/generic.component').then(m => m.GenericComponent)
+  },
+  {
+    path:"elements",
+    loadComponent: () => import('../views/element/element/element.component').then(m => m.ElementComponent)
   },
   {
     path: "**",
