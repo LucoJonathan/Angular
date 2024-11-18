24aed7c95391518a1512611741409f0c6022855b
Jonathan Luco <Jonathan.luco44@gmail.com>
2024-11-18 13:52:51 +0100

    Components

--- Dev/templateProject/src/app/app.routes.ts
+++ Dev/templateProject/src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeComponent} from '../views/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "", // Le chemin
+    component: HomeComponent // Le composant
+  }
+];
+
+
+// nomDeVariable : TypeDeVariable
