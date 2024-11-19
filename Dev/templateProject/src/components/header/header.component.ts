d2c09f02e37ac940f3f3ec002c864da61aeb9c42
Jonathan Luco <Jonathan.luco44@gmail.com>
2024-11-19 09:23:59 +0100

    Début Mardi

--- Dev/templateProject/src/components/header/header.component.ts
+++ Dev/templateProject/src/components/header/header.component.ts
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+      /*Déclaration de variables
+      *
+      *  visibilité (par défaut dans une classe : public) nomDeVariable : typeDeVariable = valeur (par défaut : undifined)
+      *il peut y avoir plusieurs types associé, et dans ce cas ils seront séparés par |
+      * */
+  /*
+  * public value: any = undefined
+  * value: any
+  * ces deux lignes sont équivalentes
+  */
+
+  multiTypeValue : number | undefined
 
 }
