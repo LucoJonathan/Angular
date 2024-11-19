a0919c6a4783f0a4a9ab296e65f3d6900bcbde74
Jonathan Luco <Jonathan.luco44@gmail.com>
2024-11-19 10:44:14 +0100

    Utilisation de variables dans l'HTML du composantÃ

--- Dev/templateProject/src/components/header/header.component.ts
+++ Dev/templateProject/src/components/header/header.component.ts
@@ -8,34 +8,34 @@ import {Component} from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  /*Déclaration de variables
+  /*/!*Déclaration de variables
   *
   *  visibilité (par défaut dans une classe : public) nomDeVariable : typeDeVariable = valeur (par défaut : undifined)
   *il peut y avoir plusieurs types associé, et dans ce cas ils seront séparés par |
-  */
-  /*
+  *!/
+  /!*
   * public value: any = undefined
   * value: any
   * ces deux lignes sont équivalentes
-  */
+  *!/
 
-  /*
+  /!*
   * multiTypeValue : number | undefined
   * optionalValue? : number
   * ces deux lignes sont équivalentes
-  */
+  *!/
 
   multiTypeValue: string | undefined
   optionalValue?: string // ajoute un ? au nom de variable équivaut à " | undefined" au niveau des types
   assertedValue!: string // ajouter un ! au nom de variable assure pour TypeScript que la valeur sera affectée et ignore 'undefined' par défaut
 
-  useNumber(value?: string) /* On peux mettre value?: le type ou alors value: le type | undefined*/ {
-    /*    if (typeof value === "string")
+  useNumber(value?: string) /!* On peux mettre value?: le type ou alors value: le type | undefined*!/ {
+    /!*    if (typeof value === "string")
         {
           console.log("value.length", value.length)
         }else{
           console.log("value.length", undefined)
-        }*/
+        }*!/
     console.log("value.length", value?.length) // remplace les lignes au dessus
   }
 
@@ -44,5 +44,8 @@ export class HeaderComponent {
     this.useNumber(this.multiTypeValue)
     this.useNumber(this.optionalValue)
     this.useNumber(this.assertedValue)
-  }
+  }*/
+
+  titre : string = "UnTitre"
+
 }
--- Dev/templateProject/src/components/sidebar/sidebar.component.ts
+++ Dev/templateProject/src/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink} from '@angular/router';
 
 @Component({
@@ -11,5 +11,5 @@ import {RouterLink} from '@angular/router';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-
+  parentValue: string = "parentValue"
 }
